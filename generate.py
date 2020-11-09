dc45b864bfbc8ae7baef24b12f8ac2535c2f972c
glzx_sunshanghai <glzx_sunshanghai@devops.com>
2020-11-09 16:41:02 +0800

    2

--- generate.py
+++ generate.py
@@ -1,6 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 import os
- 
-cmd = "sqlacodegen  mysql+pymysql://root:123456@192.168.1.14/management >tmp.py"
 
-os.system(cmd)
 
+def main(p_argv):
+    # 生成指定模型
+    # cmd = "sqlacodegen --tables account mysql+pymysql://root:123456@192.168.1.14/management >models.py"
+
+    # 生成全部模型
+    cmd = "sqlacodegen mysql+pymysql://root:123456@192.168.1.14/management >models.py"
+    os.system(cmd)
+
+    return 0
+
+
+if __name__ == '__main__':
+    start = datetime.datetime.now()
+
+    status = main(sys.argv)
+
+    elapsed = float((datetime.datetime.now() - start).seconds)
+    print("Time Used 4 All ----->>>> %f seconds" % (elapsed))
+
+    sys.exit(status)
